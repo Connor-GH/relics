f28aa8eb30784dc87bc957c9e2ec97c86f977c05
Connor <connor-gh@outlook.com>
2024-03-15 06:25:14 -0500

    mktemplate: script to autogenerate template files for C and D

---
+++ scripts/mktemplate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# make a template file
+
+[ -z "$FILEDIR" ] && FILEDIR=. 
+
+
+usage() {
+  echo "$0 [LANGUAGE] [FILENAME]"
+  echo "Where LANGUAGE is 'c' or 'd'"
+  exit 1
+}
+template() {
+  if [[ "$1" = "c" ]]; then
+    if [[ ! -z "$2" ]]; then
+      printf "#ifndef $2_H\n#define $2_H\n#endif /* $2_H */\n" > "$FILEDIR/$2.h"
+      printf "#include <$2.h>\n\n" > "$FILEDIR/$2.c"
+    fi 
+  elif [[ "$1" = "d" ]]; then
+    echo "IS D"
+    if [[ ! -z "$2" ]]; then
+      printf "module $2;\n\n" > "$FILEDIR/$2.d"
+    fi 
+  fi 
+  }
+
+  if [[ -z "$1" ]]; then
+    usage
+  fi 
+  if [[ -z "$2" ]]; then
+    usage
+  fi
+  template "$@"
